6ad645284ea7097bde758c51dbfd7299a283b2a3
hrzyusoff <hrzyusoff@gmail.com>
2018-04-28 10:38:44 +0800

    adding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './dribbble/sign-up/sign-up.component';
+import { PaymentComponent } from './dribbble/payment/payment.component';
+import { LandingPageComponent } from './dribbble/landing-page/landing-page.component';
 
+const appRoute: Routes = [
+  { path: 'signup', component: SignUpComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: 'landing', component: LandingPageComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    PaymentComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
