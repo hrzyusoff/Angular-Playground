67a6ac3bd574988b30a7c4810ab80eb80a06a9ee
hrzyusoff <harizmdyusoff@gmail.com>
2018-05-17 17:06:41 +0800

    some code adjustments

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,4 +10,63 @@ export class SidebarComponent {
 
   constructor() { }
 
+  // linkData = [
+  //   {
+  //     link: "signup",
+  //     subtitlte: "Signup"
+  //   },
+  //   {
+  //     link: "payment",
+  //     subtitlte: "Payment"
+  //   },
+  //   {
+  //     link: "landing-page",
+  //     subtitlte: "Landing Page"
+  //   },
+  //   {
+  //     link: "calculator",
+  //     subtitlte: "Calculator"
+  //   },
+  //   {
+  //     link: "app-icon",
+  //     subtitlte: "App Icon"
+  //   },
+  //   {
+  //     link: "user-profile",
+  //     subtitlte: "User Profile"
+  //   },
+  //   {
+  //     link: "setting",
+  //     subtitlte: "Setting"
+  //   },
+  //   {
+  //     link: "error-page",
+  //     subtitlte: "Error Page"
+  //   },
+  //   {
+  //     link: "music-player",
+  //     subtitlte: "Music Player"
+  //   },
+  //   {
+  //     link: "social-share",
+  //     subtitlte: "Social Share"
+  //   },
+  //   {
+  //     link: "flash-message",
+  //     subtitlte: "Flash Message"
+  //   },
+  //   {
+  //     link: "shop",
+  //     subtitlte: "Shop"
+  //   },
+  //   {
+  //     link: "direct-message",
+  //     subtitlte: "Direct Message"
+  //   },
+  //   {
+  //     link: "countdown-timer",
+  //     subtitlte: "Countdown Timer"
+  //   },
+  // ];
+
 }
