8403ac7e02f228d246b6d99d1681dc86b3386803
hrzyusoff <harizmdyusoff@gmail.com>
2018-05-25 15:59:24 +0800

    new frame

---
+++ src/app/dribbble/showcase/showcase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-showcase',
+    templateUrl: './showcase.component.html',
+    styleUrls: ['./showcase.component.css']
+})
+export class HomeShowcaseComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
